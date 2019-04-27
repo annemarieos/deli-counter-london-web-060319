9ffab8465f954fdac2b3c7d5c220bc6beacf4fbf
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-27 12:35:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,4 +9,9 @@ def line(katz_deli)
     end
     puts current_line
   end
-end   
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome #{name}. You are number #{katz_deli.length} in line."
+end 
